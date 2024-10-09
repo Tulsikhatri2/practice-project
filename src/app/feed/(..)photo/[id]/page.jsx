59ed5dcfb87ac78518748c35378d468ae98eb0f6
Tulsi Khatri <tulsiyuvasoft447@gmail.com>
2024-10-09 15:24:34 +0530

    09-10-2024

---
+++ src/app/feed/(..)photo/[id]/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const PhotoModal = ({params}) => {
+    const photo = photos.find(p=>p.id === params.id)
+  return (
+    <div>page</div>
+  )
+}
+
+export default PhotoModal   
---
+++ src/app/photo/[id]/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const PhotoPage = ({params}) => {
+    const photo = photos.find(p=>p.id === params.id)
+  return (
+    <div>page</div>
+  )
+}
+
+export default PhotoPage
